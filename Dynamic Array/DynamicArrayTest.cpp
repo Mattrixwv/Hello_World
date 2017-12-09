fdd4717f728b57f1cf8a0190900e99be6997305c
Matthew <m_ellison@ymail.com>
2017-12-09 12:20:44 -0500

    Added function calls to pop

--- Dynamic Array/DynamicArrayTest.cpp
+++ Dynamic Array/DynamicArrayTest.cpp
@@ -23,13 +23,23 @@ int main()
 	arr1.push(500, 5L);
 	for(int cnt = 0;cnt < arr1.getSize();++cnt)
 		std::cout << cnt + 1 << ". " << arr1[cnt] << '\n';
-	//std::cout << "Complete!" << std::endl;
+	std::cout << "pop(): " << arr1.pop() << std::endl;
+	std::cout << "pop(3): " << arr1.pop(3) << std::endl;
+	std::cout << "pop(0): " << arr1.pop(0) << std::endl;
+	for(int cnt = 0;cnt < arr1.getSize();++cnt)
+		std::cout << cnt + 1 << ". " << arr1[cnt] << '\n';
+
 	std::cout << "This is what happens when you try to access something out of bounds: " << arr1[50] << "\n\n\nDoubles:\n";
 
 	//Doubles
 	for(int cnt = 0;cnt < 10;++cnt)
 		arr2.push(cnt * 3.5);
 	arr2.push(500, 5L);
+	for(int cnt = 0;cnt < arr2.getSize();++cnt)
+		std::cout << cnt + 1 << ". " << arr2[cnt] << '\n';
+	std::cout << "pop(): " << arr2.pop() << std::endl;
+	std::cout << "pop(3): " << arr2.pop(3) << std::endl;
+	std::cout << "pop(0): " << arr2.pop(0) << std::endl;
 	for(int cnt = 0;cnt < arr2.getSize();++cnt)
 		std::cout << cnt + 1 << ". " << arr2[cnt] << '\n';
 	std::cout << "This is what happens when you try to access something out of bounds: " << arr2[50] << "\n\n\nCharacters:\n";
@@ -41,6 +51,11 @@ int main()
 	arr3.push("Hi", 5L);
 	for(int cnt = 0;cnt < arr3.getSize();++cnt)
 		std::cout << cnt + 1 << ". " << arr3[cnt] << '\n';
+	std::cout << "pop(): " << arr3.pop() << std::endl;
+	std::cout << "pop(3): " << arr3.pop(3) << std::endl;
+	std::cout << "pop(0): " << arr3.pop(0) << std::endl;
+	for(int cnt = 0;cnt < arr3.getSize();++cnt)
+		std::cout << cnt + 1 << ". " << arr3[cnt] << '\n';
 	std::cout << "This is what happens when you try to access something out of bounds: " << arr3[50] << std::endl;
 
 
@@ -49,7 +64,11 @@ int main()
 	arr4.push('Q', 5L);
 	for(int cnt = 0;cnt < arr4.getSize();++cnt)
 		std::cout << cnt + 1 << ". " << arr4[cnt] << '\n';
-	//std::cout << "Complete!" << std::endl;
+	std::cout << "pop(): " << arr4.pop() << std::endl;
+	std::cout << "pop(3): " << arr4.pop(3) << std::endl;
+	std::cout << "pop(0): " << arr4.pop(0) << std::endl;
+	for(int cnt = 0;cnt < arr4.getSize();++cnt)
+		std::cout << cnt + 1 << ". " << arr4[cnt] << '\n';
 	std::cout << "This is what happens when you try to access something out of bounds: " << arr4[50] << std::endl;
 
 	return 0;
