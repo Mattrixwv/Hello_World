bd8c6f479140065b276a0f0f211e7b923f3a3690
Mattrixwv <m_ellison@ymail.com>
2018-03-02 10:45:03 -0500

    Updated with multi-threading

--- Timer/TimerAvg.cpp
+++ Timer/TimerAvg.cpp
@@ -7,28 +7,62 @@
 
 #include <iostream>
 #include <chrono>
+#include <cstdint>
+#include <thread>
+#include <mutex>
 
 const int NUM_REPS = 80000;
 const int NUM_CYCLES = 10;
+const int NUM_THREADS = 8;
+
+static std::mutex barrier;
+void threadedTimer(uint64_t num, double* total);
 
 
 int main(){
 	std::chrono::high_resolution_clock::time_point begin, end;
 	std::chrono::high_resolution_clock::duration dur;
+	std::thread threads[NUM_THREADS];
 	double avg = 0;
+	uint64_t num = 0;
 
-	for(int cntCycles = 0;cntCycles < NUM_CYCLES;++cntCycles){
-		dur = std::chrono::high_resolution_clock::duration::zero();
-		for(int cntOut = 0;cntOut < NUM_REPS;++cntOut){
-			begin = std::chrono::high_resolution_clock::now();
-			for(int cnt = 0;cnt < NUM_REPS;++cnt);
-			end = std::chrono::high_resolution_clock::now();
-			dur += end - begin;
-		}
-		avg += (double)dur.count()/(double)NUM_REPS;
+	//Start all of the threads
+	for(unsigned int threadCnt = 0;threadCnt < NUM_THREADS;++threadCnt){
+		threads[threadCnt] = std::thread(threadedTimer, num, &avg);
+		++num;
 	}
-
-	std::cout << "\nAverage: " << avg/(double)NUM_CYCLES << std::endl;
+	//Join them so nothing runs off without everything else
+	for(unsigned int threadCnt = 0;threadCnt < NUM_THREADS;++threadCnt){
+		threads[threadCnt].join();
+	}
+/* */
+/*
+	uint64_t cnt = 0;
+	for(cnt = 0;cnt < NUM_THREADS;++cnt){
+		threadedTimer(&cnt, &avg);
+	}
+/* */
+	//Calculate and print the average of each 'reps' run
+	std::cout << "\nAverage: " << avg/(double)NUM_THREADS << std::endl;
 
 	return 0;
+}
+
+void threadedTimer(uint64_t num, double* total){
+	std::chrono::high_resolution_clock::time_point begin, end;
+	std::chrono::high_resolution_clock::duration dur;
+	dur == std::chrono::high_resolution_clock::duration::zero();
+
+	for(uint64_t cycleCnt = 0;cycleCnt < NUM_CYCLES;++cycleCnt){
+		begin = std::chrono::high_resolution_clock::now();
+		for(uint64_t repCnt = 0;repCnt < NUM_REPS;++repCnt){
+			//Just to make sure this thing is incrementing correctly
+			std::lock_guard<std::mutex> guard(barrier);
+			std::cout << num << ". " << cycleCnt << " : " << repCnt << std::endl;
+		}
+		end = std::chrono::high_resolution_clock::now();
+		dur += end - begin;
+	}
+	std::lock_guard<std::mutex> guard(barrier);
+	*total += (double)dur.count()/(double)NUM_CYCLES;
 }
